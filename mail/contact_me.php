4b57e4a9584fa9876c654759d72065f5685eecf4
root <root@ip-172-31-1-59.ap-southeast-2.compute.internal>
2015-09-07 16:34:35 +0000

    Correcting Contact files

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,21 +1,8 @@
 <?php
-   include("../mail/phpmailer/class.phpmailer.php");
-   include("../mail/phpmailer/class.smtp.php");
-   // Check for empty fields
-   if(empty($_POST['name'])  		||
-      empty($_POST['email']) 		||
-      empty($_POST['phone']) 		||
-      empty($_POST['message'])	||
-      !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-      {
-   	echo "No arguments Provided!";
-   	return false;
-      }
-   	
-   $name = $_POST['name'];
-   $email_address = $_POST['email'];
-   $phone = $_POST['phone'];
-   $message = $_POST['message'];
+include("phpmailer/class.smtp.php");
+include("phpmailer/class.phpmailer.php");
+
+
 
    $mail = new PHPMailer(); // create a new object
    $mail->IsSMTP(); // enable SMTP
@@ -34,5 +21,7 @@
     if(!$mail->Send())
       true;
    else
-      false; 
+      false;
+
+ 
 ?>
--- mail/gmail.php
+++ mail/gmail.php
@@ -1,32 +1,25 @@
-<?
-$account="email_address@gmail.com";
-$password="accountpassword";
-$to="mail@subinsb.com";
-$from="vishal@vishal.com";
-$from_name="Vishal G.V";
-$msg="<strong>This is a bold text.</strong>"; // HTML message
-$subject="HTML message";
+<?php
 /*End Config*/
-
+include("phpmailer/class.smtp.php");
 include("phpmailer/class.phpmailer.php");
-$mail = new PHPMailer();
-$mail->IsSMTP();
-$mail->CharSet = 'UTF-8';
-$mail->Host = "smtp.gmail.com";
-$mail->SMTPAuth= true;
-$mail->Port = 465; // Or 587
-$mail->Username= $account;
-$mail->Password= $password;
-$mail->SMTPSecure = 'ssl';
-$mail->From = $from;
-$mail->FromName= $from_name;
-$mail->isHTML(true);
-$mail->Subject = $subject;
-$mail->Body = $msg;
-$mail->addAddress($to);
-if(!$mail->send()){
- echo "Mailer Error: " . $mail->ErrorInfo;
-}else{
- echo "E-Mail has been sent";
-}
-?>
+
+
+
+   $mail = new PHPMailer(); // create a new object
+   $mail->IsSMTP(); // enable SMTP
+   $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+   $mail->SMTPAuth = true; // authentication enabled
+   $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+   $mail->Host = "smtp.gmail.com";
+   $mail->Port = 465; // or 587
+   $mail->IsHTML(true);
+   $mail->Username = "here2helpdesk@gmail.com";
+   $mail->Password = "helpyhelp2";
+   $mail->SetFrom("here2helpdesk@gmail.com");
+   $mail->Subject = "Contact Message";
+   $mail->Body = "oi";
+   $mail->AddAddress("here2helpdesk@gmail.com");
+    if(!$mail->Send())
+      true;
+   else
+      false; 
