2adad83a6b5f21351a07c3f39ace5635467f532c
nimisaya <nimisaya@outlook.com>
2015-09-17 15:44:07 +1000

    Updated user details insertion into DB

--- PHP_Process_Files/processSignup.php
+++ PHP_Process_Files/processSignup.php
@@ -1,5 +1,5 @@
 <?php
- 	// Includes pdo file 
+ 	// Includes pdo file
     include_once('../pdo.inc');
 	// Declare global variables
 	$signUpInputs = array();
@@ -16,7 +16,7 @@
 		    exit();
 		}
 	}
-	
+
 
 	// Function to search for a user with a given email and password
 	function signUp(){
@@ -26,23 +26,26 @@
 
 			if(!checkEmail()){
 				// Creates pdo query , prepare its variables and execute it in order to find a user that matches the password
-				$signUp = $pdo->prepare('INSERT INTO `USER` (`hash`,`email`, `firstName`, `lastName`, `phone`, `typeID`,`unitNumber`,`street`, `suburb`, `state`, `postcode`) VALUES (:hash, :email, :firstName, :lastName, :phone, 1, :unitNumber, :street, :suburb, :state, :postcode)');
-				$signUp->bindValue(':hash',md5($_POST['password']));
-				$signUp->bindValue(':email', $_POST['email']);
-				$signUp->bindValue(':firstName', $_POST['fname']);
-				$signUp->bindValue(':lastName', $_POST['lname']);
-				$signUp->bindValue(':phone', $_POST['pnumber']);
-				$signUp->bindValue(':unitNumber', $_POST['unumber']);
-				$signUp->bindValue(':street', $_POST['street']);
-				$signUp->bindValue(':suburb', $_POST['suburb']);
-				$signUp->bindValue(':state', $_POST['state']);
-				$signUp->bindValue(':postcode', $_POST['postcode']);
-					
-				$result = $signUp->execute();
+        $signUpAddress = $pdo->prepare('INSERT INTO 'ADDRESS' ('unitNumber', 'street', 'suburb', 'state', 'postcode') VALUES (:unitNumber, :street, :suburb, :state, :postcode)');
+        $signUpAddress->bindValue(':unitNumber', $_POST['unumber']);
+        $signUpAddress->bindValue(':street', $_POST['street']);
+        $signUpAddress->bindValue(':suburb', $_POST['suburb']);
+        $signUpAddress->bindValue(':state', $_POST['state']);
+        $signUpAddress->bindValue(':postcode', $_POST['postcode']);
+
+        $signUpUser = $pdo->prepare('INSERT INTO `USER` (`email`, `hash`, `firstName`, `lastName`, 'addressID', `phoneNo`, `typeID`) VALUES (:hash, :email, :firstName, :lastName, LAST_MODIFIED_ID, :phone, 1)');
+        $signUpUser->bindValue(':hash',md5($_POST['password']));
+        $signUpUser->bindValue(':email', $_POST['email']);
+        $signUpUser->bindValue(':firstName', $_POST['fname']);
+        $signUpUser->bindValue(':lastName', $_POST['lname']);
+        $signUpUser->bindValue(':phone', $_POST['pnumber']);
+
+        $resultAddress = $signUpAddress->execute();
+				$result = $signUpUser->execute();
 
 				// If result is different than null then creates some session variables informing user ID and type
-				if ($result) {
-					// Redirects user to index page with a success message 
+				if ($result && $resultAddress ) {
+					// Redirects user to index page with a success message
 				    header('Location: ../signin.php#signup=success');
 				    exit();
 
@@ -52,7 +55,7 @@
 				    exit();
 				}
 			}
-			
+
 		}
 		catch (PDOException $e){
 			echo $e->getMessage();
@@ -68,7 +71,7 @@
 			$checkEmailQuery->execute();
 			$result = $checkEmailQuery->fetchColumn();
 
-			if ($result != null) 
+			if ($result != null)
 				return true;
 			else
 				return false;
