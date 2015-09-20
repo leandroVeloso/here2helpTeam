b23d3096255b77ff05b4151cb31b5ef116708875
nimisaya <nimisaya@outlook.com>
2015-09-20 18:17:02 +1000

    Added lastModified to tables & fixed commenting

--- dataBase/createUserTables.sql
+++ dataBase/createUserTables.sql
@@ -1,8 +1,9 @@
 USE Helpdesk;
 
-# Create table to store users address and location of help request.
+# Delete previous versions of Address table
 DROP TABLE IF EXISTS ADDRESS;
 
+# Create table to store users address and location of help request.
 CREATE TABLE ADDRESS (
     addressID INT(4) AUTO_INCREMENT,
     unitNumber INT(10),
@@ -10,26 +11,30 @@ CREATE TABLE ADDRESS (
     suburb VARCHAR(50),
     state VARCHAR(20),
     postcode INT(4),
+    lastModified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (addressID)
 );
 
-# Create table to store user types. User may be a [1] Customer, [2] Volunteer or [3] ServiceProvider
+# Delete previous versions of Usertype table
 DROP TABLE IF EXISTS USERTYPE;
 
+# Create table to store user types. User may be a [1] Customer, [2] Volunteer or [3] ServiceProvider
 CREATE TABLE USERTYPE (
-    typeID INT,
+    typeID INT AUTO_INCREMENT,
     type VARCHAR(50) NOT NULL UNIQUE,
+    lastModified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (typeID)
 );
 
-# Insert user types.
-INSERT INTO USERTYPE (typeID, type) VALUES (1, 'Customer'),
-    (2, 'Volunteer'),
-    (3, 'Service Provider');
+# Insert user types [1] Customer, [2] Volunteer and [3] Service Provider.
+INSERT INTO USERTYPE (type) VALUES ('Customer'),
+    ('Volunteer'),
+    ('Service Provider');
 
-# Create table to store user details
+# Delete previous versions of User table
 DROP TABLE IF EXISTS USER;
 
+# Create table to store user details
 CREATE TABLE USER (
     userID INT(4) AUTO_INCREMENT,
     email VARCHAR(100) NOT NULL UNIQUE,
@@ -39,7 +44,7 @@ CREATE TABLE USER (
     addressID INT(4),
     phoneNo INT,
     typeID INT(1),
-    lastModified DATETIME,
+    lastModified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (userID),
     FOREIGN KEY (addressID) REFERENCES ADDRESS(addressID)
       ON UPDATE CASCADE
