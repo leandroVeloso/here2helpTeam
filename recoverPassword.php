315ec9dac3124ae714f419ebe002298527788af0
nimisaya <nimisaya@outlook.com>
2015-09-21 17:34:38 +1000

    Fixed wording for recover password warning

--- recoverPassword.php
+++ recoverPassword.php
@@ -53,7 +53,7 @@
                 <div class="modal-content">
                      <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title">Information doesn't match. Please try again.</h4>
+                        <h4 class="modal-title">Invalid First name, Last name or Email. Please try again.</h4>
                       </div>
                     <div class="modal-footer">
                         <a href="#" class="btn btn-primary" data-dismiss="modal">Close</a>
