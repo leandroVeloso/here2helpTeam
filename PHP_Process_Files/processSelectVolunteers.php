ae353741489f3e72f605c72c384a01fd08c75482
nimisaya <nimisaya@outlook.com>
2015-10-03 02:08:07 +1000

    Implemented function that selects users requesting volunteer accounts

--- PHP_Process_Files/processSelectVolunteers.php
+++ PHP_Process_Files/processSelectVolunteers.php
@@ -1,7 +1,10 @@
 <?php
 	$volunteers;
+	$newVolunteers;
 	selectAllVolunteers();
+	selectNewVolunteers();
 
+	// Selectes all volunteers (TypeID = 2)
 	function selectAllVolunteers(){
 		global $volunteers, $pdo;
 		try{
@@ -9,17 +12,40 @@
                                       FROM USER U INNER JOIN ADDRESS A
                                       ON U.addressID = A.addressID
                                       WHERE U.typeID = 2');
-    /*  $volunteersSelect->bindValue(':volunteerID', $_POST['userID']);
-      $volunteersSelect->bindValue(':email', $_POST['email']);
-      $volunteersSelect->bindValue(':firstName', $_POST['firstName']);
-      $volunteersSelects->bindValue(':lastName', $_POST['lastName']);
-      $volunteersSelect->bindValue(':suburb', $_POST['suburb']);
-      $volunteersSelect->bindValue(':phoneNo', $_POST['phoneNo']);
-      $volunteersSelects->bindValue(':lastModified', $_POST['lastModified']);*/
 			$volunteersSelect->execute();
 			$volunteers = $volunteersSelect->fetchAll();
 		}
 		catch (PDOException $e)
 			{echo $e->getMessage();}
 	}
+
+	// Selects users requesting volunteer accounts (TypeID = 4)
+	function selectNewVolunteers(){
+		global $newVolunteers, $pdo;
+		try{
+			$volunteersSelect = $pdo->prepare('SELECT U.userID, U.email, U.firstName, U.lastName, A.suburb, U.phoneNo, U.lastModified
+                                      FROM USER U INNER JOIN ADDRESS A
+                                      ON U.addressID = A.addressID
+                                      WHERE U.typeID = 4');
+			$volunteersSelect->execute();
+			$newVolunteers = $volunteersSelect->fetchAll();
+		}
+		catch (PDOException $e)
+			{echo $e->getMessage();}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
