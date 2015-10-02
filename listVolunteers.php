7fcd3c3d6911dd7dcc3b6623ae96b3a760116c08
nimisaya <nimisaya@outlook.com>
2015-10-03 02:09:06 +1000

    Added table to listVolunteer that shows users requesting volunteer accounts. Added email to both tables

--- listVolunteers.php
+++ listVolunteers.php
@@ -27,22 +27,26 @@
           </div>
         </div>
 
+
         <div class="row">
           <div class="col-lg-12">
             <br>
             <div class="panel panel-default">
               <div class="panel-heading">
-                  All Volunteer Accounts
+                  Requests for Volunteer Accounts
               </div>
                 <!-- /.panel-heading -->
               <div class="panel-body">
                 <div class="dataTable_wrapper">
+
+                  <!-- Create table to view list requests for volunteer accounts -->
                   <table class="table table-striped table-bordered table-hover" id="dataTables-example">
                     <thead>
                       <tr>
                         <th class="col-md-1">ID</th>
                         <th class="col-md-2">First Name</th>
                         <th class="col-md-2">Last Name</th>
+                        <th class="col-md-3">Email</th>
                         <th class="col-md-2">Phone Number</th>
                         <th class="col-md-2">Suburb</th>
                         <th class="col-md-2">Last Modified</th>
@@ -51,15 +55,16 @@
 
                     <tbody>
                       <?php
-                        foreach($volunteers as $volunteer){
-                           echo '<tr class="odd gradeX">
-                           <td>'.$volunteer['userID'].'</td>
-                           <td>'.$volunteer['firstName'].'</td>
-                           <td>'.$volunteer['lastName'].'</td>
-                           <td>'.$volunteer['phoneNo'].'</td>
-                           <td>'.$volunteer['suburb'].'</td>
-                           <td>'.date("d/m/Y", strtotime($volunteer['lastModified'])).'</td>
-                           </tr>';
+                        foreach($newVolunteers as $newVolunteer){
+                            echo '<tr class="odd gradeX">
+                            <td>'.$newVolunteer['userID'].'</td>
+                            <td><a href="viewVolunteer.php?userID='.$newVolunteer['userID'].'">'.$newVolunteer['firstName'].'</a></td>
+                            <td>'.$newVolunteer['lastName'].'</td>
+                            <td>'.$newVolunteer['email'].'</td>
+                            <td>'.$newVolunteer['phoneNo'].'</td>
+                            <td>'.$newVolunteer['suburb'].'</td>
+                            <td>'.date("d/m/Y", strtotime($newVolunteer['lastModified'])).'</td>
+                            </tr>';
                         }
                       ?>
                     </tbody>
@@ -69,6 +74,62 @@
             </div>
           </div>
         </div>
+
+
+        <div class="row">
+          <div class="col-lg-12">
+            <br>
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                  Volunteer Accounts
+              </div>
+                <!-- /.panel-heading -->
+              <div class="panel-body">
+                <div class="dataTable_wrapper">
+
+                  <!-- Create table to view list of ALL volunteers -->
+                  <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                    <thead>
+                      <tr>
+                        <th class="col-md-1">ID</th>
+                        <th class="col-md-2">First Name</th>
+                        <th class="col-md-2">Last Name</th>
+                        <th class="col-md-3">Email</th>
+                        <th class="col-md-2">Phone Number</th>
+                        <th class="col-md-2">Suburb</th>
+                        <th class="col-md-2">Last Modified</th>
+                      </tr>
+                    </thead>
+
+                    <tbody>
+                      <?php
+                        foreach($volunteers as $volunteer){
+                            echo '<tr class="odd gradeX">
+                            <td>'.$volunteer['userID'].'</td>
+                            <td><a href="viewVolunteer.php?userID='.$volunteer['userID'].'">'.$volunteer['firstName'].'</a></td>
+                            <td>'.$volunteer['lastName'].'</td>
+                            <td>'.$volunteer['email'].'</td>
+                            <td>'.$volunteer['phoneNo'].'</td>
+                            <td>'.$volunteer['suburb'].'</td>
+                            <td>'.date("d/m/Y", strtotime($volunteer['lastModified'])).'</td>
+                            </tr>';
+
+                        }
+                      ?>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+
+
       </div>
     </section>
 
@@ -77,13 +138,6 @@
         // Includes Javascript files
         include 'javascripts.inc';
     ?>
-
-
-
-
-
-
-
   </body>
 
 </html>
