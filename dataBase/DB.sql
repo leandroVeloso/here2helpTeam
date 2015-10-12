f076ba122e6aae3a8c956ae23b6a1ee0a22f5a37
nimisaya <nimisaya@outlook.com>
2015-10-12 15:28:48 +1000

    Added serviceProvider, quote & booking tables to DB

--- dataBase/DB.sql
+++ dataBase/DB.sql
@@ -11,7 +11,7 @@ DROP TABLE IF EXISTS ADDRESS;
 
 # Create table to store users address and location of help request.
 CREATE TABLE ADDRESS (
-    addressID INT(4) AUTO_INCREMENT,
+    addressID INT AUTO_INCREMENT,
     unitNumber INT(10),
     street VARCHAR(50),
     suburb VARCHAR(50),
@@ -37,14 +37,14 @@ DROP TABLE IF EXISTS USER;
 
 # Create table to store user details
 CREATE TABLE USER (
-    userID INT(4) AUTO_INCREMENT,
+    userID INT AUTO_INCREMENT,
     email VARCHAR(100) NOT NULL UNIQUE,
     hash VARCHAR(60) NOT NULL,
     firstName VARCHAR(50) NOT NULL,
     lastName VARCHAR(50) NOT NULL,
-    addressID INT(4),
+    addressID INT,
     phoneNo INT,
-    typeID INT(1),
+    typeID INT,
     lastModified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (userID),
     FOREIGN KEY (addressID) REFERENCES ADDRESS(addressID)
@@ -57,7 +57,7 @@ CREATE TABLE USER (
 
 # Create table to store service types.
 CREATE TABLE SERVICE(
-    serviceID INT(4) AUTO_INCREMENT,
+    serviceID INT AUTO_INCREMENT,
     service VARCHAR(100) NOT NULL UNIQUE,
     lastModified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY(serviceID)
@@ -65,7 +65,7 @@ CREATE TABLE SERVICE(
 
 # Create table to store priority level of help request.
 CREATE TABLE PRIORITY(
-    priorityID INT(2) AUTO_INCREMENT,
+    priorityID INT AUTO_INCREMENT,
     priority VARCHAR(50) NOT NULL UNIQUE,
     lastModified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY(priorityID)
@@ -75,7 +75,7 @@ CREATE TABLE PRIORITY(
 
 # Create table to store status of help request.
 CREATE TABLE STATUS(
-    statusID INT(1) AUTO_INCREMENT,
+    statusID INT AUTO_INCREMENT,
     status VARCHAR(50) NOT NULL UNIQUE,
     lastModified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY(statusID)
@@ -83,10 +83,10 @@ CREATE TABLE STATUS(
 
 # Create table to store help request
 CREATE TABLE REQUEST(
-    requestID INT(4) AUTO_INCREMENT,
-    clientID INT(4) NOT NULL,
-    volunteerID INT(4),
-    serviceID INT(4),
+    requestID INT AUTO_INCREMENT,
+    clientID INT NOT NULL,
+    volunteerID INT,
+    serviceID INT,
     requestName VARCHAR(50) NOT NULL,
     startDate DATE NOT NULL,
     endDate DATE,
@@ -94,10 +94,10 @@ CREATE TABLE REQUEST(
     endTime TIME,
     minPrice DECIMAL(8,2),
     maxPrice DECIMAL (8,2),
-    comment TEXT, # General information about the request
-    priorityID INT(2),
-    locationID INT(4),
-    statusID INT(1) DEFAULT 1,
+    comment VARCHAR(500), # General information about the request
+    priorityID INT,
+    locationID INT,
+    statusID INT DEFAULT 1,
     creationDate TIMESTAMP NOT NULL,
     lastModified TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (requestID),
@@ -119,6 +119,74 @@ CREATE TABLE REQUEST(
     FOREIGN KEY (statusID) REFERENCES STATUS(statusID)
       ON UPDATE CASCADE
       ON DELETE SET NULL
+);
+
+# Create table to store status of service provider details.
+CREATE TABLE SERVICEPROVIDER(
+    serviceProviderID INT AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    serviceID INT,
+    description VARCHAR(500),
+    addressID INT,
+    phoneNo INT,
+    website VARCHAR(1000),
+    lastModified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(serviceProviderID),
+    FOREIGN KEY (addressID) REFERENCES ADDRESS(addressID)
+      ON UPDATE CASCADE
+      ON DELETE SET NULL,
+    FOREIGN KEY (serviceID) REFERENCES SERVICE(serviceID)
+      ON UPDATE CASCADE
+      ON DELETE SET NULL
+);
+
+
+# Create table to store quotes.
+CREATE TABLE QUOTE(
+    quoteID INT AUTO_INCREMENT,
+    requestID INT NOT NULL,
+    serviceProviderID INT NOT NULL,
+    startDateTime DATETIME NOT NULL,
+    endDateTime DATETIME,
+    description VARCHAR(500),
+    minPrice DECIMAL(8,2) NOT NULL,
+    maxPrice DECIMAL (8,2),
+    volunteerComment VARCHAR(500),
+    clientComment VARCHAR(500),
+    creationDate TIMESTAMP NOT NULL,
+    lastModified TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(quoteID),
+    FOREIGN KEY (requestID) REFERENCES REQUEST(requestID)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE,
+    FOREIGN KEY (serviceProviderID) REFERENCES SERVICEPROVIDER(serviceProviderID)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
+
+# Create table to store bookings.
+CREATE TABLE BOOKING(
+    bookingID INT AUTO_INCREMENT,
+    quoteID INT,
+    requestID INT NOT NULL,
+    serviceProviderID INT NOT NULL,
+    startDateTime DATETIME,
+    endDateTime DATETIME,
+    description VARCHAR(500),
+    price DECIMAL(8,2),
+    comment VARCHAR(500),
+    creationDate TIMESTAMP NOT NULL,
+    lastModified TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (bookingID),
+    FOREIGN KEY (requestID) REFERENCES REQUEST(requestID)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE,
+    FOREIGN KEY (quoteID) REFERENCES QUOTE(quoteID)
+      ON UPDATE CASCADE
+      ON DELETE SET NULL,
+    FOREIGN KEY (serviceProviderID) REFERENCES SERVICEPROVIDER(serviceProviderID)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
 );
 
 # Insert user types [1] Customer, [2] Volunteer, [3] Service Provider and [4] Someone requesting a volunteer account.
@@ -174,8 +242,29 @@ VALUES ('Painting'),
 ('Building'),
 ('Driving');
 
-# Insert request
+# Insert requests
 INSERT INTO `helpdesk`.`request` (`clientID`, `serviceID`, `requestName`, `startDate`, `endDate`, `startTime`, `endTime`, `minPrice`, `maxPrice`, `comment`, `priorityID`, `locationID`, `statusID`, `creationDate`, `lastModified`)
 VALUES ('1', '1', 'Paint Roof', '2015-10-18', '2015-10-20', '12:05', '13:10', '100', '200', 'Need someone to paint my roof blue.', '2', '2', '1', NOW(), NOW()),
 ('2', '1', 'Fix toilet', '2015-10-18', '2015-10-25', '12:05', '17:10', '1500', '2000', 'My toilet is blocked.', '3', '4', '1', NOW(), NOW()),
 ('3', '5', 'Mow Yard', '2015-10-30', '2015-11-25', '05:05', '17:10', '20', '780', 'Yard is overgrown. 1 acre. Need mowing.', '3', '4', '1', NOW(), NOW());
+
+# Insert service providers.
+INSERT INTO SERVICEPROVIDER (name, serviceID, description, addressID, phoneNo, website)
+VALUES ('Bell Plumbing Maintenance', 2, 'Provide gas fitting, hot water repair and fix blocked drains. Available 24/7.', 4, '(07) 3354 3300', 'www.bellplumbing.com.au'),
+('Fallon Solutions', 2, 'Service domestic, commercial and industrial. Available 24/7.', 9, '1300 762 260', 'www.fallonsolutions.com.au'),
+('PaintPaintersPainting', 1, 'Indoor painting of houses only. Available Monday to friday, 9 to 5.', 8, '1800 999 987', 'www.paintingpaint.com.au'),
+('Cha clean', 4, 'Dry cleaning only. Open Tuesday to Saturdays 9am to 5pm.', 6, '1300 000 260', 'www.chaclean.com.au'),
+('Yellow Cabs', 10, 'Taxi service servicing South East Queensland. State preferred taxi type (standard or maxi).', NULL, '1800555432', 'www.yellowcabs.com');
+
+# Insert quotes.
+INSERT INTO QUOTE (requestID, serviceProviderID, startDateTime, endDateTime, description, minPrice, maxPrice, volunteerComment, clientComment)
+VALUES (1, 1, '2015-10-23 12:00:00', '2015-10-23 14:00:00', '1 Bedroom only', '200', '300', 'Willing to do 2 rooms for cheaper.', NULL),
+(1, 1, '2015-10-23 12:00:00', '2015-10-23 14:00:00', '2 Bedroom', '250', '400', 'Cheaper price only available until March 2015.', NULL),
+(1, 2, '2015-10-20 09:00:00', '2015-10-25 14:00:00', 'Whole House', '500', NULL, 'Client must provide the paint.', NULL),
+(1, 2, '2015-10-28 14:00:00', '2015-10-30 17:30:00', 'Toilet issue', '20', '1000', 'Price will vary. Initial consult is $20 but additional work will cost more.', NULL);
+
+
+# Insert bookings.
+INSERT INTO BOOKING (quoteID, requestID, serviceProviderID, startDateTime, endDateTime, description, price, comment)
+VALUES (1, 1, 1, '2015-10-23 12:00:00', '2015-10-23 13:00:00', 'PaintPaintings 1 Bedroom only', '200', 'Booked for the loungeroom. They will bring all materials'),
+(4, 2, 2, '2015-10-30 14:00:00', '2015-10-30 17:30:00', 'Consultation', '20', 'Booked to identify issue. Will fix on date if job isn\'t too large and provide you with a new quote');
