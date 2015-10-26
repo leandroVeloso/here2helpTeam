f925d1881a37a28f26e1a13a6adfd554e6ace3e0
Samuel Burns <sam-burns@live.com.au>
2015-10-26 19:31:17 +1000

    List new requests for volunteer ordered by zone

--- PHP_Process_Files/processSelectAvailableRequests.php
+++ PHP_Process_Files/processSelectAvailableRequests.php
@@ -5,7 +5,14 @@
 	function selectAvailableRequest(){
 		global $availableRequests, $pdo;
 		try{
-			$servicesSelect = $pdo->prepare('SELECT * FROM REQUEST R INNER JOIN PRIORITY P ON P.priorityID = R.priorityID WHERE statusID = 1');
+			$servicesSelect = $pdo->prepare('SELECT R.requestID, R.requestName, P.priorityID, R.startDate, Z.zone FROM REQUEST R INNER JOIN PRIORITY P 
+				ON R.priorityID = P.priorityID 
+				INNER JOIN ADDRESS A 
+				ON R.locationID = A.addressID 
+				INNER JOIN ZONES Z 
+				ON Z.zoneID = A.zoneID 
+				WHERE R.statusID = 1
+				ORDER BY A.zoneID');
 			$servicesSelect->execute();
 			$resultService = $servicesSelect->fetchAll();
 			$availableRequests = $resultService;
